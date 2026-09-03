57d3211da5795bc45526696ae5c78a3c2b64531e
Smit <smit.kukade.tech@gmail.com>
2026-09-03 19:06:18 +0530

    Add findMissing() Function

--- findMissing.cpp
+++ findMissing.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    //code
+int findMissing(int arr[], int size, int n) {
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int i = 0; i < size; i++) {
+        actualSum += arr[i];
+    }
+
+    return expectedSum - actualSum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 6};
+
+    cout << findMissing(arr, 5, 6) << endl;
+
     return 0;
 }
